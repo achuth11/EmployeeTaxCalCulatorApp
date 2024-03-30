11aeef2f6894706c1503829941f1bd31f95f9f87
achuth11 <97277946+achuth11@users.noreply.github.com>
2024-03-30 13:00:20 +0530

    Delete HospitalManagementService directory

--- HospitalManagementService/src/main/java/com/hospital/management/service/common/AppConstants.java
+++
@@ -1,7 +0,0 @@
-package com.hospital.management.service.common;
-
-public class AppConstants {
-
-	public static final String PATIENT_SAVED_SUCCESSFULLY = "Patient Saved Successflly.";
-	public static final String PATIENT_SAVED_FAILURE = "Unable to Save Patient. Please try again later.";
-}
